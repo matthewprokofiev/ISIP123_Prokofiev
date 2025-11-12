f114c0a5b8444af6375755f76b28a3f03acd202c
234932 <234932@KB37-118-C12>
2025-11-12 15:25:21 +0300

    1 iteration

---
+++ PR8/db.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR8
+{
+    public class DB
+    {
+        public static Prokofiev_PR8Entities Context = new Prokofiev_PR8Entities();
+    }
+}
